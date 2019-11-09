8e3892b79ed458b84f25753050e342accc18fc92
roluisker <roluisker@gmail.com>
2019-11-09 10:20:54 -0500

    min

---
+++ search/src/main/java/com/group/pow/search/SearchActivity.kt
@@ -0,0 +1,15 @@
+package com.group.pow.search
+
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.game.core.BaseActivity
+
+class SearchActivity : BaseActivity() {
+
+
+ //private lateinit var bingin: Search
+
+    //private lateinit var binding: Ac
+
+
+}
