26a0d0ba64286bd2e2701383d89d70ab498066d6
roluisker <roluisker@gmail.com>
2019-11-09 10:39:07 -0500

    SearchActivity

--- search/src/main/java/com/group/pow/search/SearchActivity.kt
+++ search/src/main/java/com/group/pow/search/SearchActivity.kt
@@ -3,13 +3,15 @@ package com.group.pow.search
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.game.core.BaseActivity
+import com.group.pow.search.databinding.ActivitySearchBinding
 
 class SearchActivity : BaseActivity() {
 
+    private lateinit var binding: ActivitySearchBinding
 
- //private lateinit var bingin: Search
-
-    //private lateinit var binding: Ac
-
-
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_search)
+        binding.lifecycleOwner = this
+    }
 }
